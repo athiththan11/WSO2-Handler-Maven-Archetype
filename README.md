14e4e4e194d8f32e7084ce64c30292fbee0efe88
Athiththan <code19111995@gmail.com>
2019-04-21 17:57:07 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# WSO2-Handler-Maven-Archetype
+# WSO2-Handler-Maven-Archetype <!-- omit in toc -->
+
 Maven Archetype for WSO2 Handlers
+
+## Usage
+
+```shell
+mvn archetype:generate -DarchetypeGroupId=com.athiththan.wso2 -DarchetypeArtifactId=wso2-handlers -DarchetypeVersion=1.0.0 -DgroupId=<Group ID>  -DartifactId=<Artifact ID>
+```
---
+++ src/main/resources/archetype-resources/src/main/java/CustomHandler.java
@@ -0,0 +1,21 @@
+package $package;
+
+import org.apache.synapse.MessageContext;
+import org.apache.synapse.rest.AbstractHandler;
+
+public class CustomHandler extends AbstractHandler {
+
+    @Override
+    public boolean handleRequest(MessageContext messageContext) {
+
+        // TODO: implement logic to handle requests ...
+        return false;
+    }
+
+    @Override
+    public boolean handleResponse(MessageContext messageContext) {
+
+        // TODO: implement logic to handle responses ...
+        return false;
+    }
+}
